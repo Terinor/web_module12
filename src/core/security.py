1ba710b5e79f91ef0f5c7e567670863ea151b3ff
Terinor <kirstruk01@gmail.com>
2024-02-20 16:19:21 +0200

    exp_time_changed

--- src/core/security.py
+++ src/core/security.py
@@ -1,10 +1,8 @@
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 SECRET_KEY = "MY_SECRET_KEY"
 ALGORITHM = "HS256"
@@ -22,9 +20,10 @@ def get_password_hash(password):
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
